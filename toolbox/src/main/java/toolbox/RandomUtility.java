f4d8a441c4f6cf72b79bf39e9ef28a0ec9c7912f
Joseph Diza <josephm.diza@gmail.com>
2022-03-30 00:39:21 -0700

    Add generateRandomBytes method to RandomUtility

--- toolbox/src/main/java/toolbox/RandomUtility.java
+++ toolbox/src/main/java/toolbox/RandomUtility.java
@@ -1,6 +1,7 @@
 package toolbox;
 
 import java.util.Random;
+import java.security.SecureRandom;
 
 public class RandomUtility {
   /**
@@ -13,5 +14,17 @@ public class RandomUtility {
     Random randGen = new Random();
     int result = randGen.nextInt(upper) + lower;
     return result;
+  }
+
+  /**
+   * Generate an array of random bytes
+   * @param len The length of the array to generate
+   * @return An array filled to len of random bytes
+  */
+  private static byte[] generateRandomBytes(int len) {
+    byte[] randomBytes = new byte[len];
+    SecureRandom sr = new SecureRandom();
+    sr.nextBytes(randomBytes);
+    return randomBytes;
   }
 }
